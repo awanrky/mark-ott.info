d40aa774ce94f46799136e77366d6047850ac662
Mark Ott <mark.ott@live.com>
2013-05-19 09:53:13 -0500

    using poet as blog engine.  Have a very very basic setup going

--- app.js
+++ app.js
@@ -10,14 +10,27 @@ var express = require('express'),
 
 var app = express();
 
+var poet = require( 'poet' )( app );
+
+poet.set({
+	postsPerPage : 3,
+	posts : './blog/_posts',
+	metaFormat : 'json'
+}).createPostRoute( '/blog/post/:post', 'blog/post' )
+	.createPageRoute( '/blog/pagination/:page', 'blog/page' )
+	.createTagRoute( '/blog/tag/:tag', 'blog/tag' )
+	.createCategoryRoute( '/blog/category/:category', 'blog/category' )
+	.init();
+
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(poet.middleware());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,10 +42,13 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/default.aspx', routes.defaultaspx);
 
-app.get('/blog', routes.blog);
+//app.get('/blog', routes.blog);
+app.get( '/blog', function ( req, res ) { res.render( 'blog/index' ) });
 
 app.get('/BigBluff', bigBluff.index);
 app.get('/BigBluff/default.aspx', bigBluff.defaultaspx); // keep old links to this page from breaking by redirecting requests for this page to the /BigBluff path
+
+
 
 http.createServer(app).listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
---
+++ blog/_posts/first-post.md
@@ -0,0 +1,3 @@
+{{{ "title" : "First Post!", "tags" : [ "first" ], "category" : "posts", "date" : "2013-05-19" }}}
+
+This is the first post!
